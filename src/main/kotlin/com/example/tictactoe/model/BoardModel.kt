86eaebb1ab16113a49c29d744861029e538a48eb
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-08-09 19:07:29 +0100

    basic code for BoardModel, provides some game logic

--- src/main/kotlin/com/example/tictactoe/model/BoardModel.kt
+++ src/main/kotlin/com/example/tictactoe/model/BoardModel.kt
@@ -1,3 +1,37 @@
 package com.example.tictactoe.model
 
-class BoardModel()
+class BoardModel(private val boardSize: Int = 3) {
+    private val board = Array(boardSize) { Array(boardSize) { Player.NONE } }
+    private var currentPlayer = Player.X
+
+    fun makeMove(row: Int, col: Int): Boolean {
+        if (row in 0 until boardSize && col in 0 until boardSize && board[row][col] == Player.NONE) {
+            board[row][col] = currentPlayer
+            currentPlayer = if (currentPlayer == Player.X) Player.O else Player.X
+            return true
+        }
+        return false
+    }
+
+    fun getCell(row: Int, col: Int): Player {
+        return if (row in 0 until boardSize && col in 0 until boardSize) board[row][col] else Player.NONE
+    }
+
+    fun checkWin(): Player {
+        // Implement win checking logic here
+        return Player.NONE
+    }
+
+    fun isBoardFull(): Boolean {
+        return board.all { row -> row.all { cell -> cell != Player.NONE } }
+    }
+
+    fun resetBoard() {
+        for (row in 0 until boardSize) {
+            for (col in 0 until boardSize) {
+                board[row][col] = Player.NONE
+            }
+        }
+        currentPlayer = Player.X
+    }
+}
---
+++ src/main/kotlin/com/example/tictactoe/model/Player.kt
@@ -0,0 +1,5 @@
+package com.example.tictactoe.model
+
+enum class Player {
+    X, O, NONE
+}
